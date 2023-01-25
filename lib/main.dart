4328581e888782754b7df54962b25261a2240d01
uros <uros2004a@gmail.com>
2023-01-25 22:43:48 +0100

    After math library

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,11 @@ void main() {
 //interakcija sa korisnikom, dok je stateless za staticne stvari
 
 class DicePage extends StatefulWidget {
-
   @override
   State<DicePage> createState() => _DicePageState();
 }
 
 class _DicePageState extends State<DicePage> {
-
   //int je definisan datatype jer je dart
   //staticki jezik za razliku od JS koji je dinamcicki
 
@@ -37,8 +35,6 @@ class _DicePageState extends State<DicePage> {
 
   @override
   Widget build(BuildContext context) {
-
-
     //Centriran je Row koji sadrzi Expaneded widget
     //koji neodzvoljava da se dogodi overflow,
     //nesto slicno kao SafeArea
@@ -50,13 +46,17 @@ class _DicePageState extends State<DicePage> {
             child: TextButton(
               // TextButton je zamena za stari FlatButton
               onPressed: () {
-                setState(() {
-                  //SetState je za promenu state, koju trigiruje
-                  //onPress
+                setState(
+                  () {
+                    //SetState je za promenu state, koju trigiruje
+                    //onPress
 
-                  LeftDiceNumber = 6;
-                  print('object');
-                });
+                    LeftDiceNumber = Random().nextInt(6) + 1;
+                    // broj od 0 do max-1
+                    //medjutim kad dodamo +1 ne moze da izbaci 0 i
+                    //broj max - 1 ostaje max sto je 6
+                  },
+                );
               },
               child: Padding(
                 padding: EdgeInsets.all(8.0),
@@ -79,5 +79,3 @@ class _DicePageState extends State<DicePage> {
     );
   }
 }
-
-
