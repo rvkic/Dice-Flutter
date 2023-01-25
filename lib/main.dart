3a3c2e5c8ad292d4ee1da9dbf19f6ac507f76203
uros <uros2004a@gmail.com>
2023-01-25 22:35:07 +0100

    in the mid in project before dart:math lib

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+//Dart;Math je biblioteka za matematicke operacije
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+//Statefull widget je za promenu stanja aplikacije
+//interakcija sa korisnikom, dok je stateless za staticne stvari
+
+class DicePage extends StatefulWidget {
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  //int je definisan datatype jer je dart
+  //staticki jezik za razliku od JS koji je dinamcicki
+
+  int LeftDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    //Centriran je Row koji sadrzi Expaneded widget
+    //koji neodzvoljava da se dogodi overflow,
+    //nesto slicno kao SafeArea
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              // TextButton je zamena za stari FlatButton
+              onPressed: () {
+                setState(() {
+                  //SetState je za promenu state, koju trigiruje
+                  //onPress
+
+                  LeftDiceNumber = 6;
+                  print('object');
+                });
+              },
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Image.asset('images/dice$LeftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  print('Right button is pressed');
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Image.asset('images/dice1.png'),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
